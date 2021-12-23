dbc06279b763a017e45c5896214b736a1bd6ab50
yilin-kim <irenek91@korea.ac.kr>
2021-12-23 17:00:49 +0900

    [frontend] App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,17 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import SignupPage from "./pages/SignupPage/Signup"
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import SignupPage from "./pages/SignupPage/Signup";
+import ChallengePage from "./pages/ChallengePage/ChallengePage";
+import DetailPage from "./pages/DetailPage";
+import MakingPage from "./pages/MakingPage";
+import TestPage from "./pages/TestPage";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <SignupPage />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Signup">
+        <Stack.Screen name="Challenge" component={ChallengePage} />
+        <Stack.Screen name="Signup" component={SignupPage} />
+        <Stack.Screen
+          name="Making"
+          component={MakingPage}
+          options={{
+            title: "",
+            headerStyle: {
+              backgroundColor: "#674BAE",
+            },
+            headerTintColor: "white",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+        <Stack.Screen name="Detail" component={DetailPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+  // return <SignupPage />;
+  // return <ChallengePage />;
+  // return <MakingPage />;
+  // return <DetailPage />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F8F6FB",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
